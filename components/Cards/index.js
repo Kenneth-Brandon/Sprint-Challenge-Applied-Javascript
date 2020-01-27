86858675b867e7c3b72bd24449e5a026f2962055
unknown <ken.brandon@aol.com>
2020-01-26 20:19:23 -0700

    cards complete

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,60 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+const cardContainer = document.querySelector(".cards-container");
+
+axios
+  .get(`https://lambda-times-backend.herokuapp.com/articles`)
+  .then(response => {
+    response.data.articles.javascript.forEach(element => {
+      cardContainer.append(article(element));
+    });
+
+    response.data.articles.bootstrap.forEach(element => {
+      cardContainer.append(article(element));
+    });
+
+    response.data.articles.technology.forEach(element => {
+      cardContainer.append(article(element));
+    });
+
+    response.data.articles.jquery.forEach(element => {
+      cardContainer.append(article(element));
+    });
+
+    response.data.articles.node.forEach(element => {
+      cardContainer.append(article(element));
+    });
+  })
+  .catch(error => {
+    console.log("Nothing to see here", error);
+  });
+
+function article(data) {
+  console.log(data);
+
+  const newArticle = document.createElement("div");
+  headline = document.createElement("div");
+  author = document.createElement("div");
+  imgDiv = document.createElement("div");
+  img = document.createElement("img");
+  authorsName = document.createElement("span");
+
+  newArticle.classList.add("card");
+  headline.classList.add("headline");
+  author.classList.add("author");
+  imgDiv.classList.add("img-container");
+
+  newArticle.append(headline);
+  newArticle.append(author);
+  author.append(imgDiv);
+  imgDiv.append(img);
+  author.append(authorsName);
+
+  headline.textContent = data.headline;
+  img.src = data.authorPhoto;
+  authorsName.innerText = data.authorName;
+
+  return newArticle;
+}
